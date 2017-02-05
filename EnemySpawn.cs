1a7857b12cfad2b53c9a55e0277edd5e6462eeb1
heroicleonardo <walkerjrtimothy1@gmail.com>
2017-02-04 19:32:13 -0500

    Add files via upload

---
+++ EnemySpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    //enemies to spawn every 60 secconds
+    public GameObject prefab;
+    public GameObject spawnLocation;
+    private Vector2 position;
+    public float seconds;
+    public GameObject newVil;
+
+    void Start()
+    {
+        position = spawnLocation.transform.position;
+        StartCoroutine(Spawn());
+
+    }
+
+    void Update()
+    {
+
+       
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(seconds);
+            newVil =  Instantiate(prefab, position, Quaternion.identity);
+            yield return new WaitForSecondsRealtime(seconds);
+            Destroy(newVil);
+        }
+            
+            
+        
+        
+    }
+}
